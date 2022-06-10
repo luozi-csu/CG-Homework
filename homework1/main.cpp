795887dd915db4fb200bb1493d5455667b4e685a
luozi-csu <2848411850@qq.com>
2022-06-11 00:24:45 +0800

    homework1 create window

---
+++ homework1/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	// glfw init
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "homework1", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "create window failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// disable cursor
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// init glew
+	glewExperimental = true;
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "glew init failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// show window
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window)) {
+		// process input
+		processInput(window);
+
+		// clear screen
+		glClearColor(1.0f, 0, 0, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
